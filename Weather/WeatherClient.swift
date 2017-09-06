dcbb1c1dedade8e0f937e4da1e79643dd62363e9
QueeniePeng <pengqueenie@gmail.com>
2017-09-06 13:17:51 -0500

    Add WeatherRequest

---
+++ Weather/WeatherClient.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherClient.swift
+//  Weather
+//
+//  Created by pengQueenie on 2017/9/6.
+//  Copyright © 2017年 pengQueenie. All rights reserved.
+//
+
+import UIKit
+
+class WeatherClient: NSObject {
+
+}
